02d6be6b94ca0cf6e230acb417700271cbdc319c
lrp39 <lrp39@case.edu>
2016-09-16 14:29:37 -0400

    added comments and changed var input to velocity

--- src/my_minimal_nodes/src/sin_commander.cpp
+++ src/my_minimal_nodes/src/sin_commander.cpp
@@ -6,24 +6,26 @@
 using namespace std;
 
 int main(int argc, char **argv) {
-    ros::init(argc, argv, "sin_commander");
-    ros::NodeHandle n; 
+    ros::init(argc, argv, "sin_commander");// name of this node will be sin commander
+    ros::NodeHandle n; // two lines to create a publisher object that can talk to ROS
     ros::Publisher my_publisher_object = n.advertise<std_msgs::Float64>("vel_cmd", 1);
-    
-    std_msgs::Float64 input_float;
+    //publishes velocity commands that the minimal controller can use
 
+    std_msgs::Float64 velocity; //the velocity that will be published
+    velocity.data = 0.0; //initalize velocity to zero
 
-    input_float.data = 0.0;
     double dt = 0.01; //10ms integration time step 
     double sample_rate = 1.0 / dt; // compute the corresponding update frequency 
-
     ros::Rate naptime(sample_rate);
-    float amplitude;
-    float frequency;
+
+    float amplitude = 0.0; //initialize the amplitude
+    float frequency=0.0; //initialize the frequency
     string input;
     
     // do work here in infinite loop (desired for this example), but terminate if detect ROS has faulted
     while(ros::ok()) {
+
+        //prompt users for amplitude
         cout<<endl;
         cout << "enter an amplitude (x to quit): ";
         //cin>>amplitude;
@@ -31,6 +33,8 @@ int main(int argc, char **argv) {
         if (input.compare("x")==0)
             return 0;
         stringstream(input) >> amplitude;
+
+        //prompt users for the frequency
         cout<<endl;
         cout << "enter a frequency (x to quit): ";
         //cin>>frequency;
@@ -38,11 +42,17 @@ int main(int argc, char **argv) {
         if (input.compare("x")==0)
             return 0;
         stringstream(input) >> frequency;
-        input_float.data = amplitude* sin(frequency * 3.14159 * dt ); 
-        my_publisher_object.publish(input_float); 
 
-        ROS_INFO("velocity = %f", input_float.data);
+        //compute the velocity from the inputs
+        velocity.data = amplitude* sin(frequency * 3.14159 * dt ); 
 
+        //publish the new velocity to topic: vel_cmd
+        my_publisher_object.publish(velocity); 
+
+        //print the velocity calculated
+        ROS_INFO("velocity = %f", velocity.data);
+
+        // wait for remainder of specified period;
         naptime.sleep();
     }
     return 0;
